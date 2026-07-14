545e8518a16442ace06bfb065127c5a54363ba51
Wahaj Ali <wahajali7181@gmail.com>
2026-07-14 11:56:17 +0500

    Fix Gemini SDK

--- ai_core/gemini_engine.py
+++ ai_core/gemini_engine.py
@@ -1,17 +1,16 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 from google import genai
 
 load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
 
-print("API KEY:", API_KEY)
+st.write("KEY FOUND:", API_KEY is not None)
+st.write("KEY LENGTH:", len(API_KEY) if API_KEY else 0)
 
-client = genai.Client(
-    api_key=API_KEY
-)
-
+client = genai.Client(api_key=API_KEY)
 
 def ask_gemini(prompt):
 
